2edc16cad16d96906f30b21ed344ec96e1891569
LLouis <ehrehrdl59@gmail.com>
2022-01-23 19:27:18 +0900

    Controller_test

---
+++ Controller_test/src/main/java/com/bs/controller/member/dao/IMemberDao.java
@@ -0,0 +1,12 @@
+package com.bs.controller.member.dao;
+
+import java.util.Map;
+
+import com.bs.controller.member.Member;
+
+public interface IMemberDao {
+	Map<String, Member> memberInsert(Member member);
+	Member memberSelect(Member member);
+	Member memberUpdate(Member member);
+	Map<String, Member> memberDelete(Member member);
+}
